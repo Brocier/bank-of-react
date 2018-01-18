c57544afd38daca0cc817b103c413e6116deb4d6
Brocier <jschoe04@gmail.com>
2018-01-18 10:31:23 -0500

    Passing the state prop with Route render

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Home from './component/Home.js'
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      accountBalance: 12312312.34
-    }
+  state = {
+    accountBalance: 485793.34
   }
 
   render() {
+
+    const HomeComponent = () => (< Home accountBalance = {
+      this.state.accountBalance
+    } />)
+
     return (
       <Router>
         <div>
           <Switch>
-            <Route exact path="/" component={Home}/>
+            <Route exact path="/" render={HomeComponent}/>
           </Switch>
         </div>
       </Router>
