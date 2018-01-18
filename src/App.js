5aa59db4effd240c59efe5ec3d75161426470e10
Brocier <jschoe04@gmail.com>
2018-01-18 10:40:15 -0500

    LINKING MADNESS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Home from './component/Home.js'
+import UserProfile from './component/UserProfile.js'
 
 class App extends Component {
   state = {
-    accountBalance: 485793.34
+    accountBalance: 485793.34,
+    currentUser: {
+      userName: 'Jim',
+      memberSince: '08/23/99'
+    }
   }
 
   render() {
@@ -12,17 +17,20 @@ class App extends Component {
     const HomeComponent = () => (< Home accountBalance = {
       this.state.accountBalance
     } />)
+    const UserProfileComponent = () => (<UserProfile
+      userName={this.state.currentUser.userName}
+      memberSince={this.state.currentUser.memberSince}/>)
 
     return (
       <Router>
         <div>
           <Switch>
             <Route exact path="/" render={HomeComponent}/>
+            <Route exact path="/userProfile" render={UserProfileComponent}/>
           </Switch>
         </div>
       </Router>
-    );
+    )
   }
 }
-
-export default App;
+export default App
