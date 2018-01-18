2d606defcaa08dcd3066d869859eb19d8aaf17d8
Brocier <jschoe04@gmail.com>
2018-01-18 10:18:46 -0500

    Following along at home

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Home from './component/Home.js'
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
-          Hello World!
-          <Home/>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+          </Switch>
         </div>
       </Router>
     );
