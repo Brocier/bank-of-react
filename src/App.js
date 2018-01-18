09f6b880abfec151861ff0e573103c47c96b160c
Brocier <jschoe04@gmail.com>
2018-01-18 10:55:07 -0500

    Adding Redirects

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Home from './component/Home.js'
 import UserProfile from './component/UserProfile.js'
+import LogIn from './component/Login.js'
 class App extends Component {
   state = {
     accountBalance: 485793.34,
@@ -10,6 +11,13 @@ class App extends Component {
       memberSince: '08/23/99'
     }
   }
+  mockLogIn = (logInInfo) => {
+    const newUser = {
+      ...this.state.currentUser
+    }
+    newUser.userName = logInInfo.userName
+    this.setState({currentUser: newUser})
+  }
   render() {
     const HomeComponent = () => (< Home accountBalance = {
       this.state.accountBalance
@@ -17,11 +25,16 @@ class App extends Component {
     const UserProfileComponent = () => (<UserProfile
       userName={this.state.currentUser.userName}
       memberSince={this.state.currentUser.memberSince}/>)
+    const LogInComponent = () => (<LogIn
+      user={this.state.currentUser}
+      mockLogIn={this.mockLogIn}
+      {...this.props}/>)
     return (
       <Router>
         <Switch>
           <Route exact path="/" render={HomeComponent}/>
           <Route exact path="/userProfile" render={UserProfileComponent}/>
+          <Route exact path="/login" render={LogInComponent}/>
         </Switch>
       </Router>
     )
