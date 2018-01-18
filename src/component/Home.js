b1ce644f1f6ce91944ac66f29145adccc222ada7
Brocier <jschoe04@gmail.com>
2018-01-18 10:16:00 -0500

    Added Home Route

---
+++ src/component/Home.js
@@ -0,0 +1,19 @@
+// src/components/Home.js
+
+import React, {Component} from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <img
+          width="200px"
+          src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png"
+          alt="bank"/>
+        <h1>Bank of React</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
